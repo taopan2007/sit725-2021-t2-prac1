06c5211fa493a1c479ae50b9c721dc40ba2c3a8d
Tao Pan <pantao@deakin.edu.au>
2021-11-21 23:01:02 +1030

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log('----> start!')
+
+var header = document.getElementById('header')
+var footer = document.getElementById('footer')
+var title = document.getElementById('title')
+var content = document.getElementById('content')
+
+// operations
+header.innerHTML = 'This is header with red border'
+footer.innerHTML = 'This is footer with blue border'
+title.innerHTML = 'Introduction'
+content.innerHTML = 'My name is Tao'
